0058f3f785dacc390252b6337b7a98a651eb8871
nandobfer <nandobfer@yahoo.com.br>
2024-01-21 22:22:58 -0300

    feat: logo navigate to /

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Avatar, Box, useMediaQuery } from "@mui/material"
+import { useNavigate } from "react-router-dom"
 
 interface LogoProps {
     size?: string
@@ -7,9 +8,7 @@ interface LogoProps {
 
 export const Logo: React.FC<LogoProps> = ({size}) => {
     const isMobile = useMediaQuery("(orientation: portrait)")
+    const navigate = useNavigate()
 
-    return (
-        <Avatar src='/logo.webp' sx={{width: size || '100%', height: 'auto'}} variant='square'>
-        </Avatar>
-    )
+    return <Avatar src="/logo.webp" sx={{ width: size || "100%", height: "auto" }} variant="square" onClick={() => navigate("/")}></Avatar>
 }
