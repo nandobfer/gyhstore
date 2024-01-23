33609fa3aa368a745d24e263a538afaef5913e1e
nandobfer <nandobfer@yahoo.com.br>
2024-01-22 21:40:23 -0300

    feat: added 404

---
+++ src/pages/Wilcard.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+import { Box, Button } from "@mui/material"
+import { Logo } from "../components/Logo"
+import { useNavigate } from "react-router-dom"
+
+interface WildcardProps {}
+
+export const Wildcard: React.FC<WildcardProps> = ({}) => {
+    const navigate = useNavigate()
+
+    return (
+        <Box sx={{ padding: "10vw", alignItems: "center", gap: "5vw" }}>
+            <Logo />
+            <Box sx={{ fontSize: "1.5rem" }}>página não encontrada</Box>
+            <Button variant="outlined" onClick={() => navigate("/")}>
+                ir para o inicio
+            </Button>
+        </Box>
+    )
+}
